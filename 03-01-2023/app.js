61537b46eaaa3e389c764f0ad15562a46aea3b43
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-03 17:10:52 +0500

    03-01-2023

---
+++ 03-01-2023/app.js
@@ -0,0 +1,30 @@
+// var number1=+prompt("Enter first number")
+// var number2=+prompt("Enter second number")
+// var sum=number1+number2
+// alert(sum)
+
+// var num=0
+// alert(num)
+// alert(num+3)
+// alert(num)
+// alert(num+7)
+// alert(num+2)
+// alert(num+2)
+// alert(num)
+// alert(num)
+// alert(num)
+// alert(num)
+// alert(num+1)
+
+// var maths=+prompt("Enter maths marks")
+// var eng=+prompt("Enter english marks")
+
+// var percent=((maths+eng)/200)*100
+// alert(percent)
+
+// var base=+prompt("Enter base value")
+// var height=+prompt("Enter height value")
+
+// var area=0.5*base*height
+// alert(area)
+
